5bf8ed1df2c7df5d0ab1b1369863cd4bcc5ebb0e
枫 <fx20031215@163.com>
2024-08-08 17:30:04 +0800

    feat: yml文件的使用

--- springboot-part-01/src/main/java/com/fengxin/Main.java
+++ springboot-part-01/src/main/java/com/fengxin/Main.java
@@ -10,9 +10,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @project springboot-part
  * @description 启动类 其他层架构在启动类同/子包下即可生效
  **/
+/*
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    @ComponentScan
+    本身是配置类
+    自动加载配置
+    扫描组件
+    组件可以加在这里
+ */
 @SpringBootApplication
 public class Main {
     public static void main (String[] args) {
+        // 创建ioc容器 加载配置
+        // 启动web服务器tomcat
         SpringApplication.run (Main.class,args);
     }
 }

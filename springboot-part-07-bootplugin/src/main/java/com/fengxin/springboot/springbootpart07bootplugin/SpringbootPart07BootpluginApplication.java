c4ce8681a0beadd6f337bb9c48b6531e5695bd5b
枫 <fx20031215@163.com>
2024-08-22 09:55:41 +0800

    feat: 添加SpringbootPart07BootpluginApplication类和pom.xml依赖描述

--- springboot-part-07-bootplugin/src/main/java/com/fengxin/springboot/springbootpart07bootplugin/SpringbootPart07BootpluginApplication.java
+++ springboot-part-07-bootplugin/src/main/java/com/fengxin/springboot/springbootpart07bootplugin/SpringbootPart07BootpluginApplication.java
@@ -3,7 +3,12 @@ package com.fengxin.springboot.springbootpart07bootplugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author FENGXIN
+ */
 @SpringBootApplication
+// scanBasePackages属性可以指定扫描包路径 但是约定大于配置 一般不建议这样做
+// @SpringBootConfiguration + @ComponentScan + @EnableAutoConfiguration = @SpringBootApplication
 public class SpringbootPart07BootpluginApplication {
     
     public static void main (String[] args) {
